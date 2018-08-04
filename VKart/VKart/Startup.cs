7638ad6e737dfcc977880f2e9dd537401bbd1517
Vipin Kumar Yadav <er.vipinyadav@gmail.com>
2018-08-05 01:58:38 +0530

    MVC in Action.

---
+++ VKart/VKart/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VKart.Startup))]
+namespace VKart
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
