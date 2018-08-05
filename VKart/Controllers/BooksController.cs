1a18124a732c8ed90defa6fb44155e60a5d29bd0
Vipin Kumar Yadav <er.vipinyadav@gmail.com>
2018-08-05 16:35:21 +0530

    First Model, Controller and View is added.

---
+++ VKart/Controllers/BooksController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VKart.Models;
+
+namespace VKart.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books/Random
+        public ActionResult Random()
+        {
+            var book = new Book
+            {
+                Name = "The White Tiger!"
+            };
+
+            return View(book);
+        }
+    }
+}
