f5f5abc6472c786f9c8f4c2f02844ad678fedba2
Vipin Kumar Yadav <er.vipinyadav@gmail.com>
2018-08-05 17:11:00 +0530

    Different Types of ActionResults in MVC.

--- VKart/Controllers/BooksController.cs
+++ VKart/Controllers/BooksController.cs
@@ -17,7 +17,27 @@ namespace VKart.Controllers
                 Name = "The White Tiger!"
             };
 
-            return View(book);
+            //return View(book);
+
+            //return Content("Hello World!");
+
+            //return HttpNotFound();
+
+            // return new EmptyResult();
+
+            // return RedirectToAction("Index","Home", new { page = 1, sortBy ="Id"});
+
+            // return Json(new { Name = "Vipin Yadav", ID = 9, DateOfBirth = new DateTime(1989, 11, 08) }, JsonRequestBehavior.AllowGet);
+
+
+            // To Download File
+            //return File(Url.Content("~/Assets/Files/testFile.txt"), "text/plain", "testFile.txt");
+
+            // To Display content on Browser
+            //byte[] fileBytes = System.IO.File.ReadAllBytes(Server.MapPath("~/Assets/Files/testFile.txt"));
+            //return File(fileBytes, "text/plain");
+
+            return Redirect("http://vipinkumaryadav.com/");
         }
     }
 }
