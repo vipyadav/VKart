aa9c09bab1ee0ee40e2f62091f671d117f0c2d7e
Vipin Kumar Yadav <er.vipinyadav@gmail.com>
2018-08-08 00:38:44 +0530

    Added more detials into Membershiptype.

--- VKart/Migrations/201808051916462_PopulateMembershipType.cs
+++ VKart/Migrations/201808051916462_PopulateMembershipType.cs
@@ -7,9 +7,9 @@ namespace VKart.Migrations
     {
         public override void Up()
         {
-            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (1, 0, 0, 0)");
-            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (2, 30, 1, 10)");
-            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (3, 90, 3, 15)");
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (1, 0, 1, 0)");
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (2, 50, 3, 10)");
+            Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (3, 100, 6, 15)");
             Sql("INSERT INTO MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (4, 300, 12, 20)");
         }
         
